baf52ec46f75da302fcfd0adf546cdf242af51b3
matheusrcruz <git.dev@outlook.com>
2018-06-29 16:54:15 -0300

    ADD: modifield

--- login.php
+++ login.php
@@ -8,5 +8,9 @@ if(empty($_POST['usuario']) || empty($_POST['senha'])){
 }
 $usuario = mysqli_real_escape_string($conexao,$_POST['usuario']);
 $senha = mysqli_real_escape_string($conexao,$_POST['senha']);
+
 $query = "select usuario_id, usuario from usuario where usuario = '{$usuario}' and senha = md5('{$senha}')";
-echo $query;exit;
+
+$result = mysqli_query($conexao,$query);
+$row = mysqli_num_rows($result);
+echo $row;exit;
