ab5d04e4b7cca83c490affd8a03a3dee512c956b
matheusrcruz <git.dev@outlook.com>
2018-06-29 15:36:03 -0300

    ADD

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="utf-8">
+        <title> first page</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="style.css">
+    </head>
+
+    <body class="Wrapper">
+        <div>         
+
+            </div>
+            <section class="corp">
+                <div class="container">
+                    <form class="box" enctype="application/x-www-form-urlencoded" action="login.php" method="POST">
+                        <img class="logo" src="http://via.placeholder.com/100x100" alt="imag">
+                        <!---email-->   
+                        <input name="usuario" class="ipp"placeholder="Digite seu usuario" type="text"><br>
+                        <!--senha-->
+                        <input name="senha" class="ipp" placeholder="Digite Sua Senha ..." type="password"><br>
+                        <button class="btn" type="submit">login </button><br>
+                        <a class="lin" href="">Esqueci minha senha?</a>
+                    </form>
+                </div>   
+            </section>
+            <footer>
+                <div class="container">
+                </div>
+            </footer>                                                                                                             
+        </div>                          
+    </body>
+</html>
+
+
+
---
+++ login.php
@@ -0,0 +1,12 @@
+<?php
+include('conexao.php');
+
+if(empty($_POST['usuario']) || empty($_POST['senha'])){
+    header('location: index.php');
+ 
+    exit();
+}
+$usuario = mysqli_real_escape_string($conexao,$_POST['usuario']);
+$senha = mysqli_real_escape_string($conexao,$_POST['senha']);
+$query = "select usuario_id, usuario from usuario where usuario = '{$usuario}' and senha = md5('{$senha}')";
+echo $query;exit;
