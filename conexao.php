0903d8ce0864d5344146f2c8929fafb07c2157c0
Matheus Rufino <31409846+matheusrcruz@users.noreply.github.com>
2018-07-02 16:24:31 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,9 +1,8 @@
 <?php
-
+// constantes, dados de acesso
 define('HOST', 'localhost');
 define('USUARIO', 'root');
 define('SENHA', '');
 define('DB', 'login');
-
 
     $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) ?? ('pagina não encontrada');
